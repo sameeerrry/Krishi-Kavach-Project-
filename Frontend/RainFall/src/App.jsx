0252dff8febd3e5974a96fa56af71604520d13a3
Dazai Twelve <98841464+DazaiTwelve@users.noreply.github.com>
2025-04-23 21:58:04 +0530

    Add files via upload

---
+++ Frontend/RainFall/src/App.jsx
@@ -0,0 +1,13 @@
+import { React} from 'react'
+import './App.css'
+import ShowData from './components/Homepage/Homepage'
+
+function App() {
+  return (
+    <>
+    <ShowData/>
+    </>
+  )
+}
+
+export default App
